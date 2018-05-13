8bad6a266b4aa509e30641ce3ea6e48c0acc1a5d
salva <tomasz.kleiner@gmail.com>
2018-05-13 11:34:16 +0200

    get  project tasks by projectid, get all projects. needs TaskModel and ProjectModel class definitions

---
+++ ScrumIt/DataAccess/TaskAccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+using NpgsqlTypes;
+using ScrumIt.Models;
+
+namespace ScrumIt.DataAccess
+{
+    internal class TaskAccess
+    {
+        //bedzie List<TaskModel> zamaist void ale jeszcze ni mo
+        public static void GetProjectTasksByProjectId(int projectid)
+        {
+            //var tasks = new List<TaskModel>();
+            using (var conn = new Connection())
+            {
+                var cmd = new NpgsqlCommand("select tsk.* from tasks tsk join sprints spr using(sprint_id) where spr.project_id = @projectid order by spr.sprint_id;")
+                {
+                    Connection = conn.Conn
+                };
+                cmd.Parameters.AddWithValue("projectid", projectid);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        /*tasks.Add(new TaskModel
+                        {
+                            pole = (rzutowanie)reader[indeks],
+                        });
+                        */
+                    }
+                }
+            }
+
+            //return tasks;
+        }
+    }
+}
---
+++ ScrumIt/DataAccess/projectAccess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+using NpgsqlTypes;
+using ScrumIt.Models;
+
+namespace ScrumIt.DataAccess
+{
+    internal class ProjectAccess
+    {
+        //bedzie List<ProjectModel> zamiast void
+        public static void GetAllProjects()
+        {
+            //var projects = new List<ProjectModel>();
+            using (var conn = new Connection())
+            {
+                var cmd = new NpgsqlCommand("select * from projects;")
+                {
+                    Connection = conn.Conn
+                };
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //projects.Add(new ProjectModel
+                        //{
+                        //    pole = (rzutowanie)reader[indeks],
+                        //});
+                    }
+                }
+            }
+
+            //return projects;
+        }
+
+
+    }
+}
+
