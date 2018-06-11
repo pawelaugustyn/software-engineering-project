24f6c0388b426bdd88845bcec7c6e68bf2eac346
Dominika <dominika.kusarek@gmail.com>
2018-06-11 19:30:38 +0200

    set picture by register functonality

--- ScrumIt/Forms/Register.cs
+++ ScrumIt/Forms/Register.cs
@@ -47,7 +47,7 @@ namespace ScrumIt.Forms
                     try
                     {
                         UserModel.Add(_user, newPasswordTextBox.Text);
-
+                        _user.Avatar = userPhotoPictureBox.Image;
                         MessageBox.Show("Pomyślnie stworzono użytkownika");
                         this.Close();
                     }
@@ -122,8 +122,7 @@ namespace ScrumIt.Forms
             {
                 try
                 {
-                    _user.Avatar = AppStateProvider.LoadImage(loadPictureDialog.FileName);
-                    userPhotoPictureBox.Image = _user.Avatar;
+                    userPhotoPictureBox.Image = AppStateProvider.LoadImage(loadPictureDialog.FileName);
 
                 }
                 catch (Exception err)
